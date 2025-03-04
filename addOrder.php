210696f433aa453a2a4a72fe8cb30e72f35d12f6
Josselin Dionisi <dionisi.josselin@gmail.com>
2025-03-04 14:55:28 +0100

    Add comments

--- addOrder.php
+++ addOrder.php
@@ -10,19 +10,22 @@ $query->bindParam('id', $id);
 $query->execute();
 $product = $query->fetch();
 
-if (isset($product)) {
+if ($product) {
 
     $total = $product['prix'];
     $createdAt = new DateTime();
     $createdAt = $createdAt->format('Y-m-d H:i:s');
 
+    //On insère d'abord la nouvelle commande avec le total et la date
     $query = $pdo->prepare('INSERT INTO orders (total, createdAt) VALUES (:total, :createdAt)');
     $query->bindParam('total', $total);
     $query->bindParam('createdAt', $createdAt);
     $query->execute();
 
+    //On récupère le dernier id en base de données qui correspond à la commande enregistrée
     $idOrder = $pdo->lastInsertId();
 
+    //On insère le produit et la commande qui vient d'être passée dans la table intermédiaire pour garder le lien produit/commande
     $query = $pdo->prepare('INSERT INTO order_product (id_product, id_order) VALUES (:id_product, :id_order)');
     $query->bindParam('id_product', $product['id']);
     $query->bindParam('id_order', $idOrder);
